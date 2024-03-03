613396152fe14284bacbf7f32a75b3f7daa036cd
WebDiyar <diyar.atashka@mail.ru>
2024-03-04 02:40:26 +0600

    testing deploying for future

--- src/pages/details/Details.tsx
+++ src/pages/details/Details.tsx
@@ -1,8 +1,8 @@
-import { useFetch } from '../../hooks/useFetch';
-import { useParams } from 'react-router-dom';
-import { MovieDetails } from '../../interfacesApi/MovieDetails';
-import { TvShowDetails } from '../../interfacesApi/TvShowDetails';
-import DetailsBanner from './detailsBanner/DetailsBanner';
+// import { useFetch } from '../../hooks/useFetch';
+// import { useParams } from 'react-router-dom';
+// import { MovieDetails } from '../../interfacesApi/MovieDetails';
+// import { TvShowDetails } from '../../interfacesApi/TvShowDetails';
+// import DetailsBanner from './detailsBanner/DetailsBanner';
 import './details.scss';
 
 const Details = () => {
@@ -12,7 +12,7 @@ const Details = () => {
 
     return (
         <div>
-            <DetailsBanner/>
+            {/* <DetailsBanner/> */}
         </div>
     )
 }
--- src/pages/details/detailsBanner/DetailsBanner.tsx
+++ src/pages/details/detailsBanner/DetailsBanner.tsx
@@ -1,55 +1,77 @@
-import React, { useState } from "react";
-import { useParams } from "react-router-dom";
-import { useSelector } from "react-redux";
-import dayjs from "dayjs";
-import "./detailsBanner.scss";
-import ContentWrapper from "../../../components/contentWrapper/ContentWrapper";
-import { useFetch } from "../../../hooks/useFetch.ts";
-import Genres from "../../../components/genres/Genres";
-import CircleRating from "../../../components/circleRating/CircleRating";
-import Img from "../../../components/lazyLoadImage/Img.jsx";
-import PosterFallback from "../../../assets/no-poster.png";
-import { PlayIcon } from "../Playbtn";
-import VideoPopup from "../../../components/videoPopup/VideoPopup";
-import { MovieDetails } from "../../../interfacesApi/MovieDetails";
-import { TvShowDetails } from "../../../interfacesApi/TvShowDetails";
+// import React, { useState } from "react";
+// import { useParams } from "react-router-dom";
+// import { useSelector } from "react-redux";
+// import dayjs from "dayjs";
+// import "./detailsBanner.scss";
+// import ContentWrapper from "../../../components/contentWrapper/ContentWrapper";
+// import { useFetch } from "../../../hooks/useFetch.ts";
+// import Genres from "../../../components/genres/Genres";
+// import CircleRating from "../../../components/circleRating/CircleRating";
+// import Img from "../../../components/lazyLoadImage/Img";
+// import PosterFallback from "../../../assets/no-poster.png";
+// import { PlayIcon } from "../Playbtn";
+// import VideoPopup from "../../../components/videoPopup/VideoPopup";
+// import { MovieDetails } from "../../../interfacesApi/MovieDetails";
+// import { TvShowDetails } from "../../../interfacesApi/TvShowDetails";
+// import { RootState } from "../../../store/store.ts";
 
 
-const DetailsBanner = () => {
-    const { mediaType, id } = useParams();
-    const { data, loading } = useFetch<MovieDetails | TvShowDetails>(`/${mediaType}/${id}`);
-    console.log(data);
+// const DetailsBanner = () => {
+//     const { mediaType, id } = useParams();
+//     const { data, loading } = useFetch<MovieDetails | TvShowDetails>(`/${mediaType}/${id}`);
+//     console.log(data);
 
-    const toHoursAndMinutes = (totalMinutes) => {
-        const hours = Math.floor(totalMinutes / 60);
-        const minutes = totalMinutes % 60;
-        return `${hours}h${minutes > 0 ? ` ${minutes}m` : ""}`;
-    };
+//     const url = useSelector((state: RootState) => state.home.url);
+
+//     const toHoursAndMinutes = (totalMinutes) => {
+//         const hours = Math.floor(totalMinutes / 60);
+//         const minutes = totalMinutes % 60;
+//         return `${hours}h${minutes > 0 ? ` ${minutes}m` : ""}`;
+//     };
     
-    return (
-        <div className="detailsBanner">
-            {!loading ? (
-                <div>
-                    Content
-                </div>
-            ) : (
-                <div className="detailsBannerSkeleton">
-                    <ContentWrapper>
-                        <div className="left skeleton"></div>
-                        <div className="right">
-                            <div className="row skeleton"></div>
-                            <div className="row skeleton"></div>
-                            <div className="row skeleton"></div>
-                            <div className="row skeleton"></div>
-                            <div className="row skeleton"></div>
-                            <div className="row skeleton"></div>
-                            <div className="row skeleton"></div>
-                        </div>
-                    </ContentWrapper>
-                </div>
-            )}
-        </div>
-    )
-}
+//     return (
+//         <div className="detailsBanner">
+//             {!loading ? (
+//                 <>
+//                     {!!data && (
+//                         <React.Fragment>
+//                             <div>
+//                                 <div className="backdrop-img">
+//                                     <Img src={url.backdrop + data?.backdrop_path} />
+//                                 </div>
+//                                 <div className="opacity-layer"></div>
+//                                 <ContentWrapper>
+//                                     <div className="content">
+//                                         <div className="left">
 
-export default DetailsBanner
+//                                         </div>
+//                                         <div className="right">
+
+//                                         </div>
+//                                     </div>
+//                                 </ContentWrapper>
+//                             </div>
+//                         </React.Fragment>
+//                     )}
+//                 </>
+//             ) : (
+//                 <div className="detailsBannerSkeleton">
+//                     <ContentWrapper>
+//                         <div className="left skeleton"></div>
+//                         <div className="right">
+//                             <div className="row skeleton"></div>
+//                             <div className="row skeleton"></div>
+//                             <div className="row skeleton"></div>
+//                             <div className="row skeleton"></div>
+//                             <div className="row skeleton"></div>
+//                             <div className="row skeleton"></div>
+//                             <div className="row skeleton"></div>
+//                         </div>
+//                     </ContentWrapper>
+//                 </div>
+//             )}
+//         </div>
+//     )
+// }
+
+// export default DetailsBanner
