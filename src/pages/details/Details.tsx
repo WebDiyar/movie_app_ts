a9ddbc87438a58018f6249e33c1df656905964b1
WebDiyar <diyar.atashka@mail.ru>
2024-03-04 02:46:21 +0600

    testing deplot for future part2

--- src/pages/details/Details.tsx
+++ src/pages/details/Details.tsx
@@ -2,7 +2,7 @@
 // import { useParams } from 'react-router-dom';
 // import { MovieDetails } from '../../interfacesApi/MovieDetails';
 // import { TvShowDetails } from '../../interfacesApi/TvShowDetails';
-// import DetailsBanner from './detailsBanner/DetailsBanner';
+import DetailsBanner from './detailsBanner/DetailsBanner';
 import './details.scss';
 
 const Details = () => {
@@ -12,7 +12,7 @@ const Details = () => {
 
     return (
         <div>
-            {/* <DetailsBanner/> */}
+            <DetailsBanner/>
         </div>
     )
 }
--- src/pages/details/detailsBanner/DetailsBanner.tsx
+++ src/pages/details/detailsBanner/DetailsBanner.tsx
@@ -1,77 +1,77 @@
-// import React, { useState } from "react";
-// import { useParams } from "react-router-dom";
-// import { useSelector } from "react-redux";
+import React from "react";
+import { useParams } from "react-router-dom";
+import { useSelector } from "react-redux";
 // import dayjs from "dayjs";
-// import "./detailsBanner.scss";
-// import ContentWrapper from "../../../components/contentWrapper/ContentWrapper";
-// import { useFetch } from "../../../hooks/useFetch.ts";
+import "./detailsBanner.scss";
+import ContentWrapper from "../../../components/contentWrapper/ContentWrapper";
+import { useFetch } from "../../../hooks/useFetch.ts";
 // import Genres from "../../../components/genres/Genres";
 // import CircleRating from "../../../components/circleRating/CircleRating";
-// import Img from "../../../components/lazyLoadImage/Img";
+import Img from "../../../components/lazyLoadImage/Img";
 // import PosterFallback from "../../../assets/no-poster.png";
 // import { PlayIcon } from "../Playbtn";
 // import VideoPopup from "../../../components/videoPopup/VideoPopup";
-// import { MovieDetails } from "../../../interfacesApi/MovieDetails";
-// import { TvShowDetails } from "../../../interfacesApi/TvShowDetails";
-// import { RootState } from "../../../store/store.ts";
+import { MovieDetails } from "../../../interfacesApi/MovieDetails";
+import { TvShowDetails } from "../../../interfacesApi/TvShowDetails";
+import { RootState } from "../../../store/store.ts";
 
 
-// const DetailsBanner = () => {
-//     const { mediaType, id } = useParams();
-//     const { data, loading } = useFetch<MovieDetails | TvShowDetails>(`/${mediaType}/${id}`);
-//     console.log(data);
+const DetailsBanner = () => {
+    const { mediaType, id } = useParams();
+    const { data, loading } = useFetch<MovieDetails | TvShowDetails>(`/${mediaType}/${id}`);
+    console.log(data);
 
-//     const url = useSelector((state: RootState) => state.home.url);
+    const url = useSelector((state: RootState) => state.home.url);
 
-//     const toHoursAndMinutes = (totalMinutes) => {
-//         const hours = Math.floor(totalMinutes / 60);
-//         const minutes = totalMinutes % 60;
-//         return `${hours}h${minutes > 0 ? ` ${minutes}m` : ""}`;
-//     };
+    // const toHoursAndMinutes = (totalMinutes) => {
+    //     const hours = Math.floor(totalMinutes / 60);
+    //     const minutes = totalMinutes % 60;
+    //     return `${hours}h${minutes > 0 ? ` ${minutes}m` : ""}`;
+    // };
     
-//     return (
-//         <div className="detailsBanner">
-//             {!loading ? (
-//                 <>
-//                     {!!data && (
-//                         <React.Fragment>
-//                             <div>
-//                                 <div className="backdrop-img">
-//                                     <Img src={url.backdrop + data?.backdrop_path} />
-//                                 </div>
-//                                 <div className="opacity-layer"></div>
-//                                 <ContentWrapper>
-//                                     <div className="content">
-//                                         <div className="left">
+    return (
+        <div className="detailsBanner">
+            {!loading ? (
+                <>
+                    {!!data && (
+                        <React.Fragment>
+                            <div>
+                                <div className="backdrop-img">
+                                    <Img src={url.backdrop + data?.backdrop_path} />
+                                </div>
+                                <div className="opacity-layer"></div>
+                                <ContentWrapper>
+                                    <div className="content">
+                                        <div className="left">
 
-//                                         </div>
-//                                         <div className="right">
+                                        </div>
+                                        <div className="right">
 
-//                                         </div>
-//                                     </div>
-//                                 </ContentWrapper>
-//                             </div>
-//                         </React.Fragment>
-//                     )}
-//                 </>
-//             ) : (
-//                 <div className="detailsBannerSkeleton">
-//                     <ContentWrapper>
-//                         <div className="left skeleton"></div>
-//                         <div className="right">
-//                             <div className="row skeleton"></div>
-//                             <div className="row skeleton"></div>
-//                             <div className="row skeleton"></div>
-//                             <div className="row skeleton"></div>
-//                             <div className="row skeleton"></div>
-//                             <div className="row skeleton"></div>
-//                             <div className="row skeleton"></div>
-//                         </div>
-//                     </ContentWrapper>
-//                 </div>
-//             )}
-//         </div>
-//     )
-// }
+                                        </div>
+                                    </div>
+                                </ContentWrapper>
+                            </div>
+                        </React.Fragment>
+                    )}
+                </>
+            ) : (
+                <div className="detailsBannerSkeleton">
+                    <ContentWrapper>
+                        <div className="left skeleton"></div>
+                        <div className="right">
+                            <div className="row skeleton"></div>
+                            <div className="row skeleton"></div>
+                            <div className="row skeleton"></div>
+                            <div className="row skeleton"></div>
+                            <div className="row skeleton"></div>
+                            <div className="row skeleton"></div>
+                            <div className="row skeleton"></div>
+                        </div>
+                    </ContentWrapper>
+                </div>
+            )}
+        </div>
+    )
+}
 
-// export default DetailsBanner
+export default DetailsBanner
